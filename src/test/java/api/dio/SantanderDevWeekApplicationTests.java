0c2450c6ea898817d0c8200b7078fdd75841b33e
MarcosSiconelli <marcossiconelli@icloud.com>
2024-06-27 12:49:26 -0300

    santander-dev-week

---
+++ src/test/java/api/dio/SantanderDevWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package api.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
